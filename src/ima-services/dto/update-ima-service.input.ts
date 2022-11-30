323d71e1f1588a256e9baae16ed6f3d90bd764a5
Giogurt <giomaster022@gmail.com>
2022-11-30 14:50:42 -0700

    ISM-33 Added new fields to service table. Added those new fields to the update query as optional

--- src/ima-services/dto/update-ima-service.input.ts
+++ src/ima-services/dto/update-ima-service.input.ts
@@ -1,22 +1,44 @@
-import { Field, Float, InputType, Int, OmitType, PartialType } from "@nestjs/graphql";
-import { CreateImaServiceInput } from "./create-ima-service.input";
+import {
+  Field,
+  Float,
+  InputType,
+  Int,
+  OmitType,
+  PartialType,
+} from '@nestjs/graphql';
+import { CreateImaServiceInput } from './create-ima-service.input';
 
 @InputType()
 export class UpdateImaServiceInput extends PartialType(
-    OmitType(CreateImaServiceInput, ['clientName', 'clientEmail', 'clientPhone', 'clientId', 'employeeId'] as const)
+  OmitType(CreateImaServiceInput, [
+    'clientName',
+    'clientEmail',
+    'clientPhone',
+    'clientId',
+    'employeeId',
+  ] as const),
 ) {
-    @Field(() => Int)
-    id: number;
+  @Field(() => Int)
+  id: number;
 
-    @Field(() => Float, {nullable: true})
-    cost?: number;
+  @Field(() => Int, { nullable: true })
+  completedPercent?: number;
 
-    @Field({nullable: true})
-    status?: string;
+  @Field(() => Float, { nullable: true })
+  cost?: number;
 
-    @Field({nullable: true})
-    departureDate?: string;
+  @Field({ nullable: true })
+  departureDate?: string;
 
-    @Field(() => Int, {nullable: true})
-    completedPercent?: number
+  @Field({ nullable: true })
+  diagnosis?: string;
+
+  @Field({ nullable: true })
+  invoiceId?: string;
+
+  @Field({ nullable: true })
+  invoiceNote?: string;
+
+  @Field({ nullable: true })
+  status?: string;
 }
